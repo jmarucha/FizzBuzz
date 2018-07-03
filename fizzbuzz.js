624f29aae87b1fcea971382fdd951bfe6f56f2f4
Jan Marucha <Jan.Marucha@softwire.com>
2018-07-03 15:16:48 +0100

    added non standard rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,43 @@
 class CustomFizzBuzz {
     constructor(ruleset) {
-        this.ruleset = ruleset || [[3,"Fizz"], [5, "Buzz"]];
+        this.ruleset = ruleset || [
+            [3, CustomFizzBuzz.simplyAdd("Fizz")],
+            [5, CustomFizzBuzz.simplyAdd("Buzz")]
+        ];
     }
     buzzify(number) {
-        let result = "";
+        let result = [];
         for (let rule of this.ruleset)
             if (number % rule[0]  == 0)
-                result += rule[1];
-        return result || number.toString();
+                rule[1](result);
+        return result.join("") || number.toString();
+    }
+    static addBefore(str, letter) {
+        return function(array) {
+            for (let i = 0; i < array.length; ++i) {
+                if (array[i].startsWith(letter)) {
+                    array.splice(i, 0, str);
+                    return;
+                } 
+            }
+            array.push(str);
+        }
+    }
+    static simplyAdd(str) {
+        return function(array) {
+            array.push(str);
+        }
+    }
+    static reverse() {
+        return function(array) {
+            array.reverse();
+        }
+    }
+    static clearAndAdd(str) {
+        return function(array) {
+            array.splice(0, array.length);
+            array.push(str);
+        }
     }
 }
 module.exports = CustomFizzBuzz;
--- index.js
+++ index.js
@@ -3,11 +3,12 @@ CustomFizzBuzz = require("./fizzbuzz.js")
 fizzBuzz = new CustomFizzBuzz;
 
 fizzBuzzBangBongFezz = new CustomFizzBuzz ([
-    [3, "Fizz"],
-    [5, "Buzz"],
-    [7, "Bang"],
-    [11, "Bong"],
-    [13, "Fezz"],
+    [3, CustomFizzBuzz.simplyAdd("Fizz")],
+    [5, CustomFizzBuzz.simplyAdd("Buzz")],
+    [7, CustomFizzBuzz.simplyAdd("Bang")],
+    [11, CustomFizzBuzz.clearAndAdd("Bong")],
+    [13, CustomFizzBuzz.addBefore("Fezz", "B")],
+    [17, CustomFizzBuzz.reverse()],
 ])
 
 
@@ -16,7 +17,7 @@ function fixedWidthString(str, width) {
     return " ".repeat(width-str.length) + str;
 }
 function printBuzzedLine(number) {
-    const width = 12;
+    const width = 14;
     let rowLabel = fixedWidthString(number.toString(), 4);
     let firstColumn = fixedWidthString(fizzBuzz.buzzify(number), width);
     let secondColumn = fixedWidthString(fizzBuzzBangBongFezz.buzzify(number), width);
@@ -24,6 +25,7 @@ function printBuzzedLine(number) {
     console.log(str);
 }
 
-for (let i = 1; i <= 500; ++i) {
+examples = [21, 33, 65, 195, 143, 255]; //examples given problem
+
+for (let i of examples)
     printBuzzedLine(i);
-}
